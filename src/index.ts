acd3cc382e69695a65db080ec6f332fee80dfe33
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-01 16:43:17 -0300

    swag34

--- src/index.ts
+++ src/index.ts
@@ -27,19 +27,17 @@ apiV1Router.use("/authenticated", Routes.authenticatedRoute());
 apiV1Router.use("/session", Routes.sessionRouter());
 
 app.use("/docs", swaggerUi.serve);
-app.get("/docs", swaggerUi.setup(swaggerFile), {
-      customSiteTitle: 'Backend Chatbot',
-      customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
-      customJs: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-      ],
-      customCssUrl: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-      ],
-});
+app.get("/docs", swaggerUi.setup(swaggerFile, {customSiteTitle: 'Backend Generator',
+  customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+  ],
+  customCssUrl: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+  ],}));
 
 // Middleware para manejar rutas no encontradas
 app.use((_req: Request, res: Response) => {
