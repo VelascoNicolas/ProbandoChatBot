5ecdc156136dbd91d1ca2b18f959ff3c6eed170b
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-01 16:28:26 -0300

    ??

--- src/docs/swagger-config.mjs
+++ src/docs/swagger-config.mjs
@@ -6,7 +6,7 @@ const doc = {
   info: {
     title: "API Danielbot",
   },
-  host: process.env.HOST || "localhost:3000",
+  host: process.env.HOST || "http://api-danielbot-mern.vercel.app",
   basePath: "/api/v1",
   schemes: process.env.HOST ? "https" : "http",
   consumes: ["application/json"],
